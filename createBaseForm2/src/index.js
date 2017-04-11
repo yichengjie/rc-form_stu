c0c5b4b0d3d3bf9ff629917d4110f08b876ffa1a
yichengjie <626659321@qq.com>
2017-04-11 09:45:19 +0800

    s

--- createBaseForm2/src/index.js
+++ createBaseForm2/src/index.js
@@ -3,13 +3,9 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
-import Helloworld from './views/Helloworld.jsx' ;
-//import HelloComp from './components/HelloComp2.jsx' ;
-//import AsyncInit from './components/async-init.js' ;
-import DepartmentList from './views/DepartmentList.jsx' ;
-import DepartmentList2 from './views/DepartmentList2.jsx' ;
+import CreateBaseFormView from './views/CreateBaseFormView.jsx' ;
 
 ReactDOM.render(
-  <DepartmentList2 />,
+  <CreateBaseFormView />,
   document.getElementById('app')
 );
