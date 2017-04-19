dc2e0cf7bdf048e6e3a3dae1e0fab6adb1700fc3
yichengjie <626659321@qq.com>
2017-04-19 16:01:07 +0800

    保存

--- ant-table1/src/index.js
+++ ant-table1/src/index.js
@@ -4,8 +4,11 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+
+import TableDemo001 from './components/TableDemo001.jsx' ;
 
 ReactDOM.render(
-  <HelloComp />,
+  <TableDemo001 />,
   document.getElementById('app')
 );
